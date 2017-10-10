b97e4588c5e92a933b1300908bc57f6dcc3cb138
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-10 22:52:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,26 +1,25 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(array)
-  cell = "   "
   separator = "|"
   dash = "-----------"
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  puts "#{cell}"
+  puts " #{array[0]} "
   puts "#{dash}"
 
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  puts "#{cell}"
+  puts " #{array[0]} "
   puts "#{dash}"
 
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  print "#{cell}"
+  print " #{array[0]} "
   print "#{separator}"
-  puts "#{cell}"
+  puts " #{array[0]} "
 end
