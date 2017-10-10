643ff94d929666a2110dabc905ac7bcd632e8cd3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-10 22:49:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(array)
-  
+  cell = "   "
+  separator = "|"
+  dash = "-----------"
+  print "#{cell}"
+  print "#{separator}"
+  print "#{cell}"
+  print "#{separator}"
+  puts "#{cell}"
+  puts "#{dash}"
+
+  print "#{cell}"
+  print "#{separator}"
+  print "#{cell}"
+  print "#{separator}"
+  puts "#{cell}"
+  puts "#{dash}"
+
+  print "#{cell}"
+  print "#{separator}"
+  print "#{cell}"
+  print "#{separator}"
+  puts "#{cell}"
 end
